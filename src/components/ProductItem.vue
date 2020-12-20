792f200c850ed27d049ce74a195f9b8085a5a64f
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-19 19:04:17 -0600

    first commit

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="product-item">
+    <img src="" alt="" />
     <p>{{ product.name }}</p>
+    <div class="product-color-container">
+      <p>Select a colour</p>
+      <img src="" alt="" />
+    </div>
+    <button class="product-item-button">+ add to proejct</button>
   </div>
 </template>
 
